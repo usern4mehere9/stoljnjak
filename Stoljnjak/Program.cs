4c6de4522d8ecca84a6e1632340bcb7146d5d571
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-27 11:35:46 +0200

    Add files via upload

---
+++ Stoljnjak/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stoljnjak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double P, r, O;
+            P = double.Parse(Console.ReadLine());
+            r = Math.Sqrt(P / Math.PI);
+            O = 2 * r * Math.PI;
+            Console.WriteLine(Math.Round(O,5));
+
+        }
+    }
+}
